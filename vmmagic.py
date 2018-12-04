2075d6c841a97e1882d1ae0c17d575b8b9af777b
Kieran <contact@kieranlondon.net>
2018-12-03 22:45:16 -0400

    ?????????

--- vmmagic.py
+++ vmmagic.py
@@ -1,43 +1,51 @@
-try:
-    from flask import Flask, render_template, request
-    import pandas
-    from datetime import date
+x=1
+print(x)
+from flask import Flask, render_template, request
+x=x+1
+print(x)
+import pandas
+x=x+1
+print(x)
+from datetime import date
 
-    app = Flask(__name__)
+app = Flask(__name__)
+x=x+1
+print(x)
 
+@app.route('/')
+def vmhome():
+    x = x + 1
+    print(x)
+    return render_template("vmhome.html")
 
-    @app.route('/')
-    def vmhome():
-        return render_template("vmhome.html")
-
-    @app.route('/results/', methods=['POST'])
-    def results():
-        try:
-            if request.method == "POST":
-                today = date.today()
-                vm = request.files["vm_file"]
-                mm = request.files["mm_file"]
-                cftpo = request.files["cftpo_file"]
-                dfvm = pandas.read_excel(vm)
-                dfmm = pandas.read_excel(mm)
-                dfcftpo = pandas.read_excel(cftpo)
-                moveoutofmm = dfmm[~dfmm.SN.isin(dfvm.SN)]
-                moveintomm = dfvm[~dfvm.SN.isin(dfmm.SN)]
-                df = dfcftpo[~dfcftpo.SN.isin(dfvm.SN)]
-                df.loc[:, "Stop CFTPO"] = today
-                df2 = dfvm[~dfvm.SN.isin(dfcftpo.SN)]
-                df2.loc[:, "Start CFTPO"] = today
-                resultscftpo = df.append(df2)
-                resultscftpo = resultscftpo.fillna('')
-                return render_template("results.html", moveoout=moveoutofmm.to_html(),
+@app.route('/results/', methods=['POST'])
+def results():
+    try:
+        if request.method == "POST":
+            today = date.today()
+            vm = request.files["vm_file"]
+            mm = request.files["mm_file"]
+            cftpo = request.files["cftpo_file"]
+            dfvm = pandas.read_excel(vm)
+            dfmm = pandas.read_excel(mm)
+            dfcftpo = pandas.read_excel(cftpo)
+            moveoutofmm = dfmm[~dfmm.SN.isin(dfvm.SN)]
+            moveintomm = dfvm[~dfvm.SN.isin(dfmm.SN)]
+            df = dfcftpo[~dfcftpo.SN.isin(dfvm.SN)]
+            df.loc[:, "Stop CFTPO"] = today
+            df2 = dfvm[~dfvm.SN.isin(dfcftpo.SN)]
+            df2.loc[:, "Start CFTPO"] = today
+            resultscftpo = df.append(df2)
+            resultscftpo = resultscftpo.fillna('')
+            return render_template("results.html", moveoout=moveoutofmm.to_html(),
                                    movein=moveintomm.to_html(), cftpo=resultscftpo.to_html())
-            else:
-                return render_template('vmhome.html')
-        except:
-            return render_template("error.html")
+        else:
+            return render_template('vmhome.html')
+    except:
+        return render_template("error.html")
 
 
-    if __name__ == '__main__':
-        app.run(debug=True)
-except:
-    print("FFFFFFUUUUUUUCK")
+if __name__ == '__main__':
+    app.run(debug=True)
+
+print("1")
